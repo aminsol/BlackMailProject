ac22e08e2126e705ed73a79967ccd9bfe99cc86d
kmholder <32622696+kmholder@users.noreply.github.com>
2018-07-21 22:54:03 -0700

    Updated PHP

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 require_once "config.php";
-?>
+$id = $_GET['id'];
 
+
+$statement = "SELECT sender, receiver, subject, message, amount FROM messages WHERE id = $id";
+$query = $db->prepare($statement);
+$query->bind_result($sender, $receiver, $subject, $message, $amount);
+$result = $query->execute();
+$query->fetch();
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,34 +20,36 @@ require_once "config.php";
     <link rel="stylesheet" href="stylesheet/bootstrap.min.css">
     <link rel="stylesheet" href="stylesheet/stylesheet.css">
 
-    <title>Hello, world!</title>
+    <title>Message Detail Page</title>
 </head>
 <body>
-  <main class="container">
-      <div class="align-middle message-ui-frame col-12">
-          <h1 class="text-center">Please respond to your message:</h1>
-              <div class="md_subject">
-                  <h3><label for="email-subject">Subject:</label></h3>
-                  <p name="from" rows="8" cols="80">This is the Subject</p>
+<main class="container">
+    <div class="align-middle message-ui-frame col-12">
+        <h1 class="text-center">Please respond to your message:</h1>
+            <div class="md_subject">
+                <h3><label for="email-subject">Subject:</label></h3>
+                <p name="from" rows="8" cols="80"><?php echo $subject;?></p>
+            </div>
+            <div class="md_email">
+                <h3><label for="email-address">Email</label></h3>
+                <p name="from" rows="8" cols="80">From: <?php echo $sender;?></p>
+            </div>
+            <div class="md_message">
+                <h3><label for="email-body">Message:</label></h3>
+                <p name="name" rows="8" cols="80"> <?php echo $message; ?></p>
+            </div>
+            <div >
+              <div class="md_image_frame">
+                    <h3><label for="email-attachment" class="email-subject">This is the picture that was uploaded:</label></h3>
+                  <img src="images/mainbg.jpg" alt=""style="justify-content: center; height: 200px; width: auto">
               </div>
-              <div class="md_email">
-                  <h3><label for="email-address">Email</label></h3>
-                  <p name="from" rows="8" cols="80">From: personThatThisIsFrom@gmail.com</p>
-              </div>
-              <div class="md_message">
-                  <h3><label for="email-body">Message:</label></h3>
-                  <p name="name" rows="8" cols="80"> The message is going to be placed here. Look at all of the blackmail that is being typed here.</p>
-              </div>
-              <div class="md_image_container">
-                <div class="md_image_frame">
-                    <h3><label for="email-attachment" class="d-block">This is the picture that was uploaded</label></h3>
-                    <img class="md_image" src="images/mainbg.jpg" alt="">
-                </div>
-              </div>
-              <button class="btn btn-danger" type="reset"> Delete and Share Image </button>
-              <button class="btn btn-primary" type="submit"> Accept and Pay </button>
-      </div>
-  </main>
+            </div>
+            <button class="btn btn-danger" type="reset"> Delete and Share Image </button>
+            <button class="btn btn-primary" type="submit"> Accept and Pay $<?php echo $amount?></button>
+    </div>
+</main>
+
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -50,5 +59,6 @@ require_once "config.php";
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
 <script src="/js/bootstrap.min.js"></script>
+
 </body>
 </html>
