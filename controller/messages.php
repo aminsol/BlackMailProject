8182575c926cd62541bb466b13fe4cff897d9991
Amin Soltani <solamin.amin@gmail.com>
2018-07-21 19:21:31 -0700

    Messaging backend

--- controller/messages.php
+++ controller/messages.php
@@ -8,21 +8,28 @@
 require_once '../config.php';
 
 if (isset($_POST['createMessage'])) {
-    $statement = "insert into messages (sender, receiver, subject, message, amount) values (?, ?, ?, ?, ?)";
+    $statement = "insert into messages (sender, receiver, subject, message, amount, attachment) values (?, ?, ?, ?, ?, ?)";
     $query = $db->prepare($statement);
-    $query->bind_param('ssssi', $_SESSION['userEmail'], $_POST['receiver'], $_POST['subject'], $_POST['message'], $_POST['amount']);
-    if ($query->execute()) {
-        $_SESSION['message'] = "Your message was successfully sent";
-        $_SESSION['notify'] = "Success";
-        $imageInfo = getimagesize($_FILES['attachment']['tmp_name']);
-        if ($imageInfo !== false) {
-            $imageName = $query->insert_id . image_type_to_extension($imageInfo[2]);
-            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $uploadFolder . $imageName);
+    $imageInfo = getimagesize($_FILES['attachment']['tmp_name']);
+    if ($imageInfo !== false) {
+        $imageName = uniqid() . image_type_to_extension($imageInfo[2]);
+        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $uploadFolder . $imageName)) {
+            $query->bind_param('ssssis', $_SESSION['userEmail'], $_POST['receiver'], $_POST['subject'],
+                $_POST['message'], $_POST['amount'], $imageName);
+            if ($query->execute()) {
+                $_SESSION['message'] = "Your message was successfully sent";
+                $_SESSION['notify'] = "Success";
+            } else {
+                $_SESSION['message'] = "There was a problem with creating your message in the database!";
+                $_SESSION['notify'] = "Error";
+            }
         }
-    } else {
-        $_SESSION['message'] = "There was a problem with creating your message in the database! ";
-        $_SESSION['notify'] = "Error";
+        else{
+            $_SESSION['message'] = "We were unable to upload your file!";
+            $_SESSION['notify'] = "Error";
+        }
     }
+
     header('Location: ../composeMessage.php');
     exit;
 }
