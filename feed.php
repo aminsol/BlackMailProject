7e0e6b6bc7d9c3466b8ad2dce8f922bf5cd3deb7
Chris Meneses <meneses.christopher@gmail.com>
2018-07-23 21:54:46 -0700

    Update feed.php

--- feed.php
+++ feed.php
@@ -13,44 +13,93 @@ require_once "config.php";
 </head>
 
 <body>
-  <div class= "col-sm-12">
+  <!-- <h1>Danny's Blackmail</h1>  -->
+  <div class="inbox-ui-frame col-12">
+    <aside class="left-row">
+      <div class="inbox-Logo">
+        <h5><a href ="#">Danny's <br>BLACKMAIL</a></h5>
+      </div>
+    </aside>
+  </div>
+
+  <div class= "row">
     <ul class="other-buttons">
-        <li><a href="inbox.php" class="btn-link">Inbox</a></li>
-        <li><a href="outbox.php" class="btn-link">Outbox</a></li>
-        <li><a href="#">Important</a></li>
-        <li><a href="#">Drafts</a></li>
-        <li><a href="#">Trash</a></li>
+        <a href="inbox.php" class="btn-link">Inbox</a>
+        <a href="outbox.php" class="btn-link"><br>Outbox</a>
+        <a href="#"><br>Important</a>
+        <a href="#"><br>Drafts</a>
+        <a href="#"><br>Trash</a>
     </ul>
-</div>
+
 <div class="container">
-  <h2>Don't let this be you!</h2>
+
   <div class="row">
     <div class="col-md-4">
       <div class="thumbnail">
-        <a href="img1.jpg" target="_blank">
+        <a href="uploadFiles/img1.jpg" target="_blank">
           <img src="img1.jpg" style="width:100%">
           <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+            <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
           </div>
         </a>
       </div>
     </div>
+    <div class="col-md-4">
+        <div class="thumbnail">
+          <a href="uploadFiles/img1.jpg" target="_blank">
+            <img src="img1.jpg" style="width:100%">
+              <div class="caption">
+                <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+              </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <a href="uploadFiles/img1.jpg" target="_blank">
+            <img src="img1.jpg" style="width:100%">
+              <div class="caption">
+                <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+              </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <a href="uploadFiles/img1.jpg" target="_blank">
+            <img src="img1.jpg" style="width:100%">
+              <div class="caption">
+                <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+              </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <a href="uploadFiles/img1.jpg" target="_blank">
+            <img src="img1.jpg" style="width:100%">
+              <div class="caption">
+                <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+              </div>
+          </a>
+        </div>
+      </div>
     <div class="col-md-4">
       <div class="thumbnail">
         <a href="img2" target="_blank">
-          <img src="img2.jpg" alt="Nature" style="width:100%">
+          <img src="uploadFiles/img2.jpg" style="width:100%">
           <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+            <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
           </div>
         </a>
       </div>
     </div>
     <div class="col-md-4">
       <div class="thumbnail">
-        <a href="img3.jpg" target="_blank">
-          <img src="img3.jpg" alt="Fjords" style="width:100%">
+        <a href="uploadFiles/img3.jpg" target="_blank">
+          <img src="img3.jpg" style="width:100%">
           <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+            <p class="text-black-50">Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
           </div>
         </a>
       </div>
